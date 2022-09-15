5bdfed7db3c76484aefae10291d89f960b7719d0
kamiloPROO <fotocopiasa100@gmail.com>
2022-09-15 09:03:49 -0500

    melo todo

--- src/components/FooterComponent.vue
+++ src/components/FooterComponent.vue
@@ -1,11 +1,19 @@
+/* eslint-disable */
+// @ is an alias to /src
 <template>
   <div>
     <footer>
 
 <div id="nosotros">
-    <div class=" container-fluid">
+    <div class="qr">
+      <div>
+      <img class="ww" src="../assets/qr_download.png"/>
+      </div>
       <div class="map">
         <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3986.0733013783824!2d-76.56465938482123!3d2.4826874981908746!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x8e300410eb607c65%3A0x614545787e90bea6!2sSENA!5e0!3m2!1ses-419!2sco!4v1660662008619!5m2!1ses-419!2sco" width="600" height="450" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
+      </div>
+      <div>
+      <img class="www" src="../assets/qrq.png"/>
       </div>
     </div>
 </div>
@@ -95,6 +103,23 @@ footer {
   width: 30px;
   height: 30px;
   margin-left: 40px;
+}
+
+.qr{
+  display: flex;
+}
+.ww{
+  width: 60%;
+  margin-top: 15%;
+  margin-left: 30px;
+}
+.www{
+  width: 53%;
+  margin-top: 15%;
+  margin-left: 90px;
+}
+.map{
+  margin-left: 6%;
 }
 
 @media (max-width: 682px) {
